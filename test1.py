721cea3e8f67a8120cea2e800f6dad2fca3ce3cb
ChenPW1128 <53811540+ChenPW1128@users.noreply.github.com>
2022-09-26 10:07:03 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -4,10 +4,10 @@ if __name__ == '__main__':
     re = "domain1.com"
     num = 0
 
-    with open("log.txt") as file:
-        for line in file:
+    with open("log.txt") as file:   # 从文件中读取日志数据
+        for line in file:           # 逐行进行判断
             if re in line:
-                num = num + 1
+                num = num + 1       # 满足条件，num加1
             else:
                 continue
 
